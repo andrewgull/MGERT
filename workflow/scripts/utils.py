ad6a1d2d4e5a75bdaa1fbafae4ddf7abfa1b619d
Andrei Guliaev <andrei.guliaev@imbim.uu.se>
2025-05-26 17:50:27 +0200

    feat: add get_path_using_name()

--- workflow/scripts/utils.py
+++ workflow/scripts/utils.py
@@ -27,3 +27,19 @@ def read_genome_names(file_path: str) -> pd.DataFrame:
         return df["name"].tolist()
     except Exception as e:
         raise ValueError(f"Error reading genome names from {file_path}: {e}")
+
+def get_path_using_name(name: str, file_path: str) -> str:
+    """
+    Get the path of a genome assembly using its name.
+    arguments:
+        name (str): The name of the genome.
+        file_path (str): Path to the TSV file containing genome names and paths.
+    returns:
+        str: The path of the genome assembly.
+    """
+    df = pd.read_csv(file_path, sep="\t", header=None, names=["name", "path"])
+    row = df[df["name"] == name]
+    if not row.empty:
+        return row["path"].values[0]
+    else:
+        raise ValueError(f"Genome name '{name}' not found in {file_path}.")
